8967de66cc6f6708c5c48f875c49086ff839b0e0
Xenia <xenia.melman@gmail.com>
2024-01-30 22:42:01 +0100

    getting day names

--- aoc_stats_scraper.py
+++ aoc_stats_scraper.py
@@ -4,19 +4,42 @@ import pandas as pd
 import time
 
 def collect_yearly_stats(years):
-    stats = pd.DataFrame({'date': [], 'both': [], 'one': []})
+    """
+    Outer loop gets day stats, inner loop day names and leaderboard stats
+    """
+    stats = pd.DataFrame({'date': [], 'both': [], 'one': [], 'name': []})
     for year in years:
-        url = "https://adventofcode.com/" + str(year) + "/stats"
-        data = requests.get(url)
-        soup = BeautifulSoup(data.text, 'html.parser')
+        print('collecting data for year ' + str(year))
+        stats_url = "https://adventofcode.com/" + str(year) + "/stats"
+        stats_data = requests.get(stats_url)
+        soup = BeautifulSoup(stats_data.text, 'html.parser')
         table = soup.find_all(attrs={'class' : 'stats'})
         for entry in table:
+            i = 25
             for line in entry:
                 if line == '\n':
                     continue
                 info = line.find_all(string=True)
-                temp_df = pd.DataFrame({'date': info[0].strip()+".12."+str(year), 'both': int(info[1].strip()), 'one': int(info[3].strip())}, index=[0])
+
+                # dayname 
+                dayname_url = "https://adventofcode.com/" + str(year) + "/day/" + str(i)
+                dayname_data = requests.get(dayname_url)
+                day_soup = BeautifulSoup(dayname_data.text, 'html.parser')
+                day_name_list = day_soup.find_all('h2')
+                day_name = day_name_list[0].next.replace('--- ', '').replace(' ---', '').split(':')[1]
+
+                # leaderboard stats (todo)
+
+
+                # combine
+                temp_df = pd.DataFrame({'date': info[0].strip()+".12."+str(year), 
+                                        'both': int(info[1].strip()), 
+                                        'one': int(info[3].strip()), 
+                                        'name': day_name}, index=[0])
                 stats = pd.concat([stats,temp_df],ignore_index=True)
+                print(temp_df)
+                i -= 1
+                time.sleep(1)
         
         # no spamming :) 
         time.sleep(2)
